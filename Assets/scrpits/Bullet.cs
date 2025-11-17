f3935f7b42cc38fd4cadce83bc58709d27fde7db
npg108 <npg108@student.aru.ac.uk>
2025-11-17 14:23:50 +0000

    cannon tower aoe added

--- Assets/scrpits/Bullet.cs
+++ Assets/scrpits/Bullet.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Bullet : MonoBehaviour
 {
 
     private Transform target;
-
+    public GameObject impactEffect;
+    public float explostionRadius;
     public float speed = 70f;
+    
+
     public void Seek (Transform _target)
     {
         target = _target;
@@ -31,17 +35,56 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
 
     }
 
    private void HitTarget()
    {
-        Debug.Log("WE HIT SOMETHING!");
+      
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+
+        if (explostionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
         Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explostionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+               
+                Damage(collider.transform);
+            }
 
 
+        }
+        
+        
 
-   }
-
+    }
     
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+   
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explostionRadius);
+    }
+
+
 }
